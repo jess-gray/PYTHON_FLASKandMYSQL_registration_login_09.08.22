5a91956c2ffe6d04f8a74a05eaa247e09d6b76c7
jess_gray <jac.cooper32@gmail.com>
2022-09-09 15:52:21 -0400

    Homework complete with notes

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 
@@ -11,10 +11,10 @@ def home_page():
 
 @app.route('/register_user', methods = ['POST']) #this is to actually register a user
 def register_user():
-    print(request.form)
+    print(request.form) #print statement should be on all post methods to make sure the information is being taken in 
     if not User.validate_create(request.form): #validation needs to happen BEFORE hashing 
         return redirect('/')
-    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    pw_hash = bcrypt.generate_password_hash(request.form['password']) #hashing - make sure Bcrpyt is installed/imported 
     print(pw_hash)    
     data = {
         'first_name' : request.form['first_name'],
@@ -23,5 +23,35 @@ def register_user():
         'password' : pw_hash
     }
     user_id = User.create(data)
-    session['user_id'] = user_id #this key is IMPORTANT
+    session['user_id'] = user_id #this key is IMPORTANT to hold information (Result of an insert query)
     return redirect('/dashboard')
+
+@app.route('/login_user', methods = ['POST']) #this is to login a user
+def login():
+    print(request.form)
+    data = {
+        "email" : request.form['email']
+        }
+    user_in_db = User.get_by_email(data)
+    if not user_in_db: #checks to see if email is correct
+        flash('Invalid Email/password')
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']): #checks to see if password is correct
+        flash('Invalid Email/password')
+        return redirect('/')
+    session['user_id'] = user_in_db.id #this is result of select query 
+    return redirect('/dashboard')
+
+@app.route('/dashboard') #this is dashbaord/welcome for user
+def dashboard():
+    if "user_id" not in session: #if user is not logged in this will take them back to the login/register page 
+        return redirect('/')
+    data = {
+        'id' : session['user_id']
+    }
+    return render_template('dashboard.html', active_user = User.get_by_id(data))
+
+@app.route('/logout') #this is to logout
+def logout():
+    session.clear() #this clears the session 
+    return redirect('/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -22,6 +22,22 @@ class User:
         print(results)
         return results
     
+    @classmethod #this is to read one users info
+    def get_by_id(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+        results = connectToMySQL('adding_users').query_db(query, data)
+        print(results)
+        return cls(results[0])
+    
+    @classmethod #this is to get user info by email 
+    def get_by_email(cls, data):
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+        results = connectToMySQL('adding_users').query_db(query, data)
+        print(results)
+        if results == (): #this is to help validate the email address is not in db
+            return False
+        return cls(results[0])
+    
     @staticmethod #this is validating a new user registering 
     def validate_create(reqForm):
         is_valid = True
@@ -34,11 +50,17 @@ class User:
         if not EMAIL_REGEX.match(reqForm['email']): #make sure import re/REGEX is added for this to work 
             flash('Invalid email address')
             is_valid = False
+        data = {
+            'email' : reqForm['email']
+        }
+        user_in_db = User.get_by_email(data) #this is to check to see if email is already in database
+        if user_in_db:
+            flash('Email already registered')
         if len(reqForm['password']) < 8:
             flash('Password is too short!')
         if (reqForm['password']) != (reqForm['password_confirm']): #this is to confirm passwords are the same. password_confirm came from HTML form
             flash('Passwords do not match!')
             is_valid = False
         return is_valid
-        #need to add in if email is in database 
+        
 
